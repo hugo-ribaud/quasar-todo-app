4122c366ad230703f41da4ea831e7ab20a498e4d
Hugo <hugo.ribaud@epitech.eu>
2021-09-22 16:02:01 +0200

    todo app

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+   <h5 class="q-mt-none">Help</h5>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ab nostrum. Assumenda inventore quaerat et tenetur repellendus similique autem provident ipsam hic? Enim saepe reiciendis quod placeat necessitatibus quisquam doloremque!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iste reiciendis inventore! Ipsam facilis, sapiente quasi magni asperiores veritatis nobis ipsa laborum aut provident culpa sed laudantium quam, quas enim!</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  
+})
+</script>
